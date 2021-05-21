8276e657f99fee5468ad38f5a7cb4f9126c32c59
SEOPSS\prero <comet00096@gmail.com>
2021-05-21 14:35:47 +0900

    feat: Create on login page

---
+++ Front/vue/src/components/news.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>뉴스페이지입니다.</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- Front/vue/src/router/index.js
+++ Front/vue/src/router/index.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import EmpRegist from '../components/regist.vue';
-import EmpList from '../components/list.vue';
-import EmpDetail from '@/components/detail.vue';
+
+import HomePage from '../components/home.vue';
+import MapSearch from '../components/mapsearch.vue';
+import KeySearch from '../components/keysearch.vue';
+import NoticePage from '@/components/notice.vue';
+import NewsPage from '@/components/news.vue';
+
+import JoinPage from '@/components/user/join.vue';
+import LoginPage from '@/components/user/login.vue';
 //버전 2에서만 가능한 @별칭경로 // 상위경로 웹펙엣
 Vue.use(VueRouter);
 
@@ -11,26 +17,49 @@ export default new VueRouter({
     mode: 'history',
     routes: [
         {
-            path: '/detail',
+            path: '/',
             // 요청경로로오면
-            name: 'datail',
-            component: EmpDetail,
+            name: 'homena',
+            component: HomePage,
             //컴포넌츠 연결
         },
         {
-            path: '/list',
-            name: 'list',
-            component: EmpList,
+            path: '/mapsearch',
+            // 요청경로로오면
+            name: 'mapsearch',
+            component: MapSearch,
+            //컴포넌츠 연결
         },
         {
-            path: '/regist',
-            name: 'regist',
-            component: EmpRegist,
+            path: '/keysearch',
+            name: 'keysearch',
+            component: KeySearch,
+        },
+        {
+            path: '/notice',
+            name: 'notice',
+            component: NoticePage,
+        },
+        {
+            path: '/news',
+            name: 'news',
+            component: NewsPage,
         },
         {
             path: '*',
             name: 'notfound',
             component: { template: '<div>not found</div>' },
+        },
+        //==========user
+        {
+            path: '/user/join',
+            name: 'join',
+            component: JoinPage,
+        },
+        {
+            path: '/user/login',
+            name: 'login',
+            component: LoginPage,
         },
     ],
 });
